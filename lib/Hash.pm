acde2318c5c1f729967400868c2b2a3c7d7bc154
brian d foy <brian.d.foy@gmail.com>
2006-05-18 02:58:14 +0000

    * updates for distro and kwalitee
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@1801 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/Hash.pm
+++ lib/Hash.pm
@@ -152,7 +152,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT
 
-Copyright 2002-2005, brian d foy, All Rights Reserved
+Copyright 2002-2006, brian d foy, All Rights Reserved
 
 You may use, modify, and distribute this under the same terms
 as Perl itself.
