865b84689d87b6463a5515719244fa1dc7b3714f
brian d foy <brian.d.foy@gmail.com>
2022-01-08 21:58:06 -0500

    bump version

--- lib/Test/Data.pm
+++ lib/Test/Data.pm
@@ -3,7 +3,7 @@ use 5.008;
 package Test::Data;
 use strict;
 
-our $VERSION = '1.243';
+our $VERSION = '1.244';
 
 use Carp qw(carp);
 
--- lib/Test/Data/Scalar.pm
+++ lib/Test/Data/Scalar.pm
@@ -13,7 +13,7 @@ our @EXPORT = qw(
 	string_between_ok
 	);
 
-our $VERSION = '1.243';
+our $VERSION = '1.244';
 
 use Scalar::Util;
 use Test::Builder;
